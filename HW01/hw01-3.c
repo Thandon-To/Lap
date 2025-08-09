2cbdf18b17f88931b41c5f6dbd431c1b35aaee3c
Thandon-To <thanadonlungta@gmail.com>
2025-08-09 17:27:48 +0700

    Add files via upload

---
+++ HW01/hw01-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    char Name [50];
+    char Last  [50];
+    printf( "First Name: " );
+    scanf("%s", Name);
+    printf( "Last Name: " );
+    scanf("%s", Last);
+    printf("%s",Name );
+    printf(" %s",Last);
+    printf(" TC, RMUTL, Chiang Mai, Thailand");
+}
