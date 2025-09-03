87d7225b392e89582a4e526cd2cadf68dd3ba6f9
Thandon-To <thanadonlungta@gmail.com>
2025-09-03 14:10:10 +0700

    Update midterm02.c

--- midterm/midterm02.c
+++ midterm/midterm02.c
@@ -1,36 +1,29 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
     int n;
-    int i, j;
-    int isPrime;
-
     printf("Enter number :\n");
-    if (scanf("%d", &n) != 1) return 0;
+    if (scanf("%d", &n) != 1) {
+        printf("Please Enter number\n");
+        return 0;
+    }
 
-    i = n;
-    do {
-        if (i >= 2) {                  /* กันกรณี n < 2 ไม่ให้พิมพ์อะไร */
-            isPrime = 1;
-
-            j = 2;
-            if (j * j <= i) {          /* มีเหตุให้เช็กอย่างน้อยหนึ่งครั้งค่อยเข้า do-while */
+    int i = n;
+    if (i >= 2) {
+        do {
+            int j = 2;
+            if (j * j <= i) {        
                 do {
-                    if (i % j == 0) {
-                        isPrime = 0;
-                        break;         /* ออกจากลูปในทันที */
-                    }
+                    if (i % j == 0) break;  
                     j++;
                 } while (j * j <= i);
             }
-
-            if (isPrime) {
+            if (j * j > i) {           
                 printf("%d ", i);
             }
-        }
-        i--;
-    } while (i >= 2);                  /* เงื่อนไขตรวจที่ท้ายลูปนอก */
-
+            i--;
+        } while (i >= 2);
+    }
     printf("\n");
     return 0;
 }
